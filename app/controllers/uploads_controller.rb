6f77b72f8ccb26ad66b4dc90b32393472c187210
a0ki-github <aoki.hotaka@gmail.com>
2021-09-06 16:06:41 +0900

    Add: redirect先を設定

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -5,6 +5,7 @@ class UploadsController < ApplicationController
   def create
     upload_file = params[:room_image]
     base64_image = Base64.strict_encode64(upload_file.read)
+    redirect_to '/uploads/show'
   end
 
   def show
