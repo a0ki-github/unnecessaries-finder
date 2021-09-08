0d47b971467bb3f2049c5bd7aeec4be41b71c840
a0ki-github <aoki.hotaka@gmail.com>
2021-09-08 09:42:39 +0900

    Add: APIキーを追加しAPIリクエストを有効化

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,18 +1,15 @@
 require 'net/http'
 
 class UploadsController < ApplicationController
-  before_action :require_image
+  before_action :require_image, only: :create
 
   def new
   end
 
   def create
-    upload_file = params[:room_image]
-    base64_image = Base64.strict_encode64(upload_file.read)
-
-    headers = {
-      "Content-Type" => "application/json"
-    }
+    base64_image = Base64.strict_encode64(params[:room_image].read)
+    api_key = Rails.application.credentials.gcp[:vision_api][:api_key]
+    headers = { "Content-Type" => "application/json" }
 
     body = { 
       requests: [
@@ -31,10 +28,12 @@ class UploadsController < ApplicationController
     }.to_json
 
     response = Net::HTTP.post(
-      URI('https://vision.googleapis.com/v1/images:annotate'),
+      URI("https://vision.googleapis.com/v1/images:annotate?key=#{api_key}"),
       body,
       headers
     )
+
+    debugger
 
     if response.code == '200'
       redirect_to '/uploads/show'
