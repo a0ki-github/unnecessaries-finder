ca26044ead3fe47107c47a38a067ea1f441b274a
a0ki-github <aoki.hotaka@gmail.com>
2021-09-09 14:40:28 +0900

    Add: Itemモデルを作成

---
+++ db/migrate/20210909051343_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :why_release
+
+      t.timestamps
+    end
+  end
+end
