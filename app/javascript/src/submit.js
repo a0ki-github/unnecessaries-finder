387532f59ded1f8e3ad3608a93e9c53f9396dbd3
a0ki-github <aoki.hotaka@gmail.com>
2021-09-13 15:54:33 +0900

    Add: submit.jsで/uploadsへのPOSTリクエスト送信を記載（なぜかcreate.jsの処理が実行されたない不具合あり）

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -1,5 +1,5 @@
 submitImage = () => {
-  // プレビュー
+  // プレビュー機能
   const target = document.querySelector("#room_image")
   const file = target.files[0];
   const reader = new FileReader();
@@ -13,4 +13,11 @@ submitImage = () => {
   if (file) {    
     reader.readAsDataURL(file);
   }
+
+  // POSTリクエスト
+  const xhr = new XMLHttpRequest();
+  const form = document.getElementById('image_form');
+  const formData = new FormData(form);
+  xhr.open("POST", "/uploads", true);
+  xhr.send(formData);
 }
