4ca19a03493ec1089687b86be16b41ea9de2fdb5
a0ki-github <aoki.hotaka@gmail.com>
2021-09-13 14:02:45 +0900

    Add: 画像のプレビュー機能を実装

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -1,3 +1,16 @@
 submitImage = () => {
-  console.log('hello');
+  // プレビュー
+  const target = document.querySelector("#room_image")
+  const file = target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    const preview = document.querySelector("#preview")
+    
+    if (preview) {
+      preview.src = reader.result;
+    }
+  }
+  if (file) {    
+    reader.readAsDataURL(file);
+  }
 }
