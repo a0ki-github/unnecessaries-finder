5a23dfea86b506ce0758de69e0fa397d9d275004
a0ki-github <aoki.hotaka@gmail.com>
2021-09-14 21:32:11 +0900

    Modify: submitImage()における画像送信アクションを、submitボタンのクリックからajaxリクエストの送信に変更

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -1,10 +1,10 @@
 submitImage = () => {
   // プレビュー機能
-  const target = document.querySelector("#room_image");
+  const target = document.getElementById("room_image");
   const file = target.files[0];
   const reader = new FileReader();
   reader.onloadend = () => {
-    const preview = document.querySelector("#preview");
+    const preview = document.getElementById("preview");
     
     if (preview) {
       preview.src = reader.result;
@@ -15,16 +15,14 @@ submitImage = () => {
   }
 
   // 画像送信機能
-  document.querySelector("#submit_button").click();
-  
-  // 以下コードではなぜかcreate.js.erbを参照できずUnknownFormatエラーが発生。
-  // const form = document.getElementById('image_form');
-  // form.submit();
-
-  // 以下コードではエラーは起きないがなぜかcreate.js.erbの内容がビューに反映されず。
-  // const xhr = new XMLHttpRequest();
-  // const form = document.getElementById('image_form');
-  // const formData = new FormData(form);
-  // xhr.open("POST", "/uploads", true);
-  // xhr.send(formData);
+  const form = document.getElementById('image_form');
+  const formData = new FormData(form);
+  $.ajax({
+    url: form.action,
+    type: form.method,
+    data: formData,
+    processData: false,
+    contentType: false,
+    cache: false
+  });
 }
