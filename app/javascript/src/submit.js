f13023548e0788ab5a32bf9513319df64c00074d
a0ki-github <aoki.hotaka@gmail.com>
2021-09-14 15:36:13 +0900

    Add: submitボタンを透明にし、formのonchangeでクリックされるよう設定

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -14,10 +14,16 @@ submitImage = () => {
     reader.readAsDataURL(file);
   }
 
-  // POSTリクエスト
-  const xhr = new XMLHttpRequest();
-  const form = document.getElementById('image_form');
-  const formData = new FormData(form);
-  xhr.open("POST", "/uploads", true);
-  xhr.send(formData);
+  // 画像送信機能
+  document.querySelector("#submit_button").click();
+  
+  // 以下コードではなぜかcreate.js.erbを参照できずUnknownFormatエラーが発生。
+  // const form = document.getElementById('image_form');
+  // xhr.send(formData);
+
+  // 以下コードではエラーは起きないがなぜかcreate.js.erbの内容がビューに反映されず。
+  // const xhr = new XMLHttpRequest();
+  // const formData = new FormData(form);
+  // xhr.open("POST", "/uploads", true);
+  // xhr.send(formData);
 }
