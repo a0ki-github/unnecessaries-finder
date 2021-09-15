ab0266db2558bc20c85cf6d1ab5fa79a6027ca8f
a0ki-github <aoki.hotaka@gmail.com>
2021-09-16 08:46:39 +0900

    Add: admin/itemsのscaffold_controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'uploads#new'
+  namespace :admin do
+    resources :items
+  end
   resources :uploads, only: %i(new create show)
+  root 'uploads#new'
 end
