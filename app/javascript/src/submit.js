4d756a2ca28a9fad369e70a6dca8e2dd3290b10e
a0ki-github <aoki.hotaka@gmail.com>
2021-09-21 14:44:50 +0900

    Fix:診断結果の初期化処理をcreate.js.erbからsubmit.jsに移管 Add:Loadingエフェクトを追加

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -1,4 +1,7 @@
 submitImage = () => {
+  // 初期化
+$("#result").remove();
+
   // プレビュー機能
   const target = document.getElementById("room_image");
   const file = target.files[0];
@@ -13,6 +16,12 @@ submitImage = () => {
   if (file) {    
     reader.readAsDataURL(file);
   }
+
+  // Loading
+  $(".container").append('<div class="spinner-grow text-success m-1" role="status"></div>');
+  $(".container").append('<div class="spinner-grow text-danger m-1" role="status"></div>');
+  $(".container").append('<div class="spinner-grow text-warning m-1" role="status"></div>');
+  $(".container").append('<div class="spinner-grow text-info m-1" role="status"></div>');
 
   // 画像送信機能
   const form = document.getElementById('image_form');
