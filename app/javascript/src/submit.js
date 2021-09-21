ab97123af4269825726e73c2e30409f1be62f0f6
a0ki-github <aoki.hotaka@gmail.com>
2021-09-21 17:24:38 +0900

    Modify: Loadingエフェクト追加のリファクト

--- app/javascript/src/submit.js
+++ app/javascript/src/submit.js
@@ -17,11 +17,13 @@ $("#result").remove();
     reader.readAsDataURL(file);
   }
 
-  // Loading
-  $(".container").append('<div class="spinner-grow text-success m-1" role="status"></div>');
-  $(".container").append('<div class="spinner-grow text-danger m-1" role="status"></div>');
-  $(".container").append('<div class="spinner-grow text-warning m-1" role="status"></div>');
-  $(".container").append('<div class="spinner-grow text-info m-1" role="status"></div>');
+  // Loadingエフェクト
+  $(".container").append(
+    '<div class="spinner-grow text-success m-1" role="status"></div>\
+    <div class="spinner-grow text-danger m-1" role="status"></div>\
+    <div class="spinner-grow text-warning m-1" role="status"></div>\
+    <div class="spinner-grow text-info m-1" role="status"></div>'
+  );
 
   // 画像送信機能
   const form = document.getElementById('image_form');
