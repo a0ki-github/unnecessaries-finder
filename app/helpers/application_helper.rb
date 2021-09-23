e76892e8dea4e6d2f878d704becf5bf83c42ce3d
a0ki-github <aoki.hotaka@gmail.com>
2021-09-23 09:16:44 +0900

    Add: メタタグの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'いらないモノ診断',
+      description: 'あなたの部屋の「実はいらないモノ」が見つかるアプリ',
+      charset: 'utf-8',
+      og: {
+        site_name: :site,
+        title: :site,
+        type: 'website',
+        description: 'あなたの部屋の「実はいらないモノ」が見つかるアプリ',
+        url: request.url,
+        image: nil,
+        locale: 'ja_JP'
+      }
+    }
+  end
 end
