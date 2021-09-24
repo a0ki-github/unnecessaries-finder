aefd32cd679fa8ec313295051f63ef46b5ffbb2e
a0ki-github <aoki.hotaka@gmail.com>
2021-09-24 13:53:13 +0900

    Add: プライバシーポリシーのページを作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def terms_of_use
     set_meta_tags title: '利用規約'
   end
+
+  def privacy_policy
+    set_meta_tags title: 'プライバシーポリシー'
+  end
 end
