ae3dc632b6a721428488be49fc9c4001253ff93b
a0ki-github <aoki.hotaka@gmail.com>
2021-09-24 09:11:22 +0900

    Add: TwitterシェアURLを作成

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,2 +1,9 @@
 module UploadsHelper
+  def twitter_share_url(detected_items)
+    link = request.url
+    sentence = "次のモノを手放すべきか、ご意見・コメントお待ちしてます%0a"
+    items = detected_items.map {|detected_item| "・" + detected_item + "%0a"}.join
+
+    return "https://twitter.com/share?text=#{sentence + items}&url=#{link}"
+  end
 end
