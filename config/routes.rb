4f84c530278046804691b1f7286ba97d791a7caa
a0ki-github <aoki.hotaka@gmail.com>
2021-09-25 07:23:32 +0900

    Fix: SessionsControllerをAdmin::SessionsControllerに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   get '/terms_of_use', to: 'static_pages#terms_of_use'
   get '/privacy_policy', to: 'static_pages#privacy_policy'
   namespace :admin do
     resources :items
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
   resources :uploads, only: %i(new create show)
   root 'uploads#new'
