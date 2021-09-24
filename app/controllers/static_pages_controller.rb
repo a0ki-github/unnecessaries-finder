3926cd74acdefa18bc901954efb58d3bf36818cd
a0ki-github <aoki.hotaka@gmail.com>
2021-09-24 11:44:48 +0900

    Add: StaticPages controller&viewを作成

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def terms_of_use
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/terms_of_use', to: 'static_pages#terms_of_use'
   namespace :admin do
     resources :items
   end
