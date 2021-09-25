353b72f2361bcb82d3f7c517d71269812cb0aeba
a0ki-github <aoki.hotaka@gmail.com>
2021-09-25 19:05:06 +0900

    Add: admin/sessions#createを定義、ログインフォームにsessionスコープを追加、パスワードをcredentialsに保管

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,4 +1,18 @@
 class Admin::SessionsController < ApplicationController
+  include Admin::SessionsHelper
+
   def new
+  end
+
+  def create
+    password = params[:session][:admin_password]
+    if admin_authenticate(password)
+      session[:admin_password] = digest(password)
+      debugger
+      redirect_to admin_items_path, success: '管理者としてログインしました'
+    else
+      flash.now[:danger] = 'パスワードが間違っています'
+      render :new
+    end
   end
 end
--- app/helpers/admin/sessions_helper.rb
+++ app/helpers/admin/sessions_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SessionsHelper
+  def admin_authenticate(password)
+    digest(password) == Rails.application.credentials.admin[:password_digest]
+  end
+
+  def digest(string)
+    Digest::MD5.hexdigest(string)
+  end
 end
