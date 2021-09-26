d6ce96f2c476a8b33c0ab061d7a8475e5b7ef0fc
a0ki-github <aoki.hotaka@gmail.com>
2021-09-26 11:22:36 +0900

    Add: feedbacks#createと#showを実装

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -5,10 +5,24 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.new
   end
 
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      redirect_to feedbacks_path, success: '問い合わせが送信されました'
+    else
+      flash.now[:danger] = '問い合わせが送信できませんでした'
+      render :new
+    end
+  end
+
   def show
   end
 
   private
+
+  def feedback_params
+    params.require(:feedback).permit(:kind, :content)
+  end
 
   def set_title
     set_meta_tags title: 'お問い合わせ'
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,3 @@
 class Feedback < ApplicationRecord
-  enum kind: { 'アプリについてのご意見・お問い合わせ': 0, '不具合・エラー報告': 1, 'その他': 2 }
+  enum kind: { 'アプリについてのご意見やご質問': 0, '不具合・エラー報告': 1, 'その他': 2 }
 end
