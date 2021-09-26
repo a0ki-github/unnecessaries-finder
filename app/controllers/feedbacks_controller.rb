844bb66909c12c91ed17a5218031e056e5aa4bf2
a0ki-github <aoki.hotaka@gmail.com>
2021-09-26 10:39:51 +0900

    Add: お問い合わせフォームを作成

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,16 @@
 class FeedbacksController < ApplicationController
+  before_action :set_title
+
   def new
+    @feedback = Feedback.new
   end
 
   def show
+  end
+
+  private
+
+  def set_title
+    set_meta_tags title: 'お問い合わせ'
   end
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,3 @@
 class Feedback < ApplicationRecord
+  enum kind: { 'アプリについてのご意見・お問い合わせ': 0, '不具合・エラー報告': 1, 'その他': 2 }
 end
