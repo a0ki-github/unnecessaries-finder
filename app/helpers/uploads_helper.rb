ee52903a6d64bcacc9ce0f716cfa9f9e96b547e8
a0ki-github <aoki.hotaka@gmail.com>
2021-10-01 11:44:55 +0900

    Add: Twitterシェアにハッシュタグを追加

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,9 +1,10 @@
 module UploadsHelper
   def twitter_share_url(detected_items)
-    link = request.url
-    sentence = "次のモノを手放すべきか、ご意見・コメントお待ちしてます%0a"
-    items = detected_items.map {|detected_item| "・" + detected_item + "%0a"}.join
+    link = request.url + "%0a"
+    sentence = "これらは手放すべき？%0aご意見・コメントお待ちしてます%0a"
+    items = detected_items.map {|detected_item| "・" + detected_item + "%0a"}.join + "%0a"
+    hashtags = "いらないモノ診断,ミニマリスト"
 
-    return "https://twitter.com/share?text=#{sentence + items}&url=#{link}"
+    return "https://twitter.com/share?text=#{sentence + items}&url=#{link}&hashtags=#{hashtags}"
   end
 end
