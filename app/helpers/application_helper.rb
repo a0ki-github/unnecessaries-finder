04050ef85561ffd721f3926aaa87458e8bca4870
a0ki-github <aoki.hotaka@gmail.com>
2021-10-01 11:22:25 +0900

    Add: og:imageを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'いらないモノ診断',
-      description: 'あなたの部屋の「実はいらないモノ」が見つかるアプリ',
+      description: '「実はいらないモノ」が見つかるアプリ',
       charset: 'utf-8',
       reverse: true,
       icon: [
@@ -13,10 +13,18 @@ module ApplicationHelper
         site_name: :site,
         title: :site,
         type: 'website',
-        description: 'あなたの部屋の「実はいらないモノ」が見つかるアプリ',
+        description: '「実はいらないモノ」が見つかるアプリ',
         url: request.url,
-        image: nil,
+        image: image_url('room.jpg'),
         locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: {
+          _: image_url('room.jpg'),
+          width: 300,
+          height: 300
+        }
       }
     }
   end
