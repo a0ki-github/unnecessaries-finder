ce6dafdfaf42922e90ad97dd602eb5e7130f4f65
a0ki-github <aoki.hotaka@gmail.com>
2021-12-20 23:37:15 +0900

    Modify: base64エンコード処理をプライベートメソッドに移動

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -5,13 +5,17 @@ class UploadsController < ApplicationController
   end
 
   def create
-    @room_image = RoomImage.new(base64_encoded: Base64.strict_encode64(params[:room_image].read))
+    @room_image = RoomImage.new(base64_encoded: base64_encoded) 
     if @room_image.save
       @items = Item.filled.detected(@room_image.detected_items)
     end
   end
 
   private
+
+  def base64_encoded
+    Base64.strict_encode64(params[:room_image].read)
+  end
 
   def register_items
     @room_image.detected_items.each {|item| Item.find_or_create_by(name: item) }
