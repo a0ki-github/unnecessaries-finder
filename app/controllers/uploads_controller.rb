226b7c2c4057b6c8f70f38d77aa9c612c202fee0
a0ki-github <aoki.hotaka@gmail.com>
2021-12-21 08:53:09 +0900

    Modify: リダイレクト文とフラッシュ文を統合

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,16 +1,15 @@
 class UploadsController < ApplicationController
   after_action :add_items, only: :create
 
-  def new
-  end
+  def new; end
 
   def create
     @room_image = RoomImage.new(base64_encoded: base64_encoded)
+
     if @room_image.save
       @items = Item.filled.detected(@room_image.detected_items)
     else
-      redirect_to root_path
-      flash[:danger] = '予期せぬエラーが発生しました。開発者にお問い合わせください。'
+      redirect_to root_path, danger: '予期せぬエラーが発生しました。開発者にお問い合わせください。'
     end
   end
 
