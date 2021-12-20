d2b18637e0b211a6dbdbf2765c48a7b497278474
a0ki-github <aoki.hotaka@gmail.com>
2021-12-21 07:23:50 +0900

    Add: APIリクエストが失敗した場合のフラッシュメッセージを追加

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,16 @@
 class UploadsController < ApplicationController
-  after_action :register_items, only: :create
+  after_action :add_items, only: :create
 
   def new
   end
 
   def create
-    @room_image = RoomImage.new(base64_encoded: base64_encoded) 
+    @room_image = RoomImage.new(base64_encoded: base64_encoded)
     if @room_image.save
       @items = Item.filled.detected(@room_image.detected_items)
+    else
+      redirect_to root_path
+      flash[:danger] = '予期せぬエラーが発生しました。開発者にお問い合わせください。'
     end
   end
 
@@ -17,7 +20,7 @@ class UploadsController < ApplicationController
     Base64.strict_encode64(params[:room_image].read)
   end
 
-  def register_items
-    @room_image.detected_items.each {|item| Item.find_or_create_by(name: item) }
+  def add_items
+    @room_image.detected_items&.each {|item| Item.find_or_create_by(name: item) }
   end
 end
