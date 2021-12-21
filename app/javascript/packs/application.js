cc96b06f912a559fa3c62f4bf87ea087c6487c20
a0ki-github <aoki.hotaka@gmail.com>
2021-12-21 11:07:33 +0900

    Fix: 記述の整理

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("jquery")
 import "../src/submit"
+
+require("jquery")
 
 Rails.start()
 Turbolinks.start()
