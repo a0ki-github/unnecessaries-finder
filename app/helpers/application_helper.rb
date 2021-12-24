d6bb05ab5ecbf1e85025939b1c6aafc404141369
a0ki-github <aoki.hotaka@gmail.com>
2021-12-24 11:59:12 +0900

    Modify: TwitterシェアURLの生成ロジックをuploadヘルパーからapplicationヘルパーに移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,12 @@ module ApplicationHelper
   def admin_url?
     request.url.include?('/admin')
   end
+
+  def twitter_share_url
+    link = request.protocol + request.host + "%0a"
+    sentence = "このアプリで「実はいらないモノ」が見つかりました。%0a"
+    hashtags = "いらないモノ診断,ミニマリスト"
+
+    return "https://twitter.com/share?text=#{sentence}&url=#{link}&hashtags=#{hashtags}"
+  end
 end
