34bb402e94105018cc9b43f6da92352b1b3a98a6
a0ki-github <aoki.hotaka@gmail.com>
2021-12-24 12:09:12 +0900

    Modify: Twitterシェアの文言を見直し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,7 +35,7 @@ module ApplicationHelper
 
   def twitter_share_url
     link = request.protocol + request.host + "%0a"
-    sentence = "このアプリで「実はいらないモノ」が見つかりました。%0a"
+    sentence = "いらないものが見つかりました。%0a"
     hashtags = "いらないモノ診断,ミニマリスト"
 
     return "https://twitter.com/share?text=#{sentence}&url=#{link}&hashtags=#{hashtags}"
