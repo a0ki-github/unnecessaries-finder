fe4ccedf6b0803b186cdce69576bea13bd73f6b5
a0ki-github <aoki.hotaka@gmail.com>
2022-01-07 06:03:10 +0900

    Add: サイトマップの元データ作成

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://www.unnecessaries-finder.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -24,4 +24,7 @@ SitemapGenerator::Sitemap.create do
   #   Article.find_each do |article|
   #     add article_path(article), :lastmod => article.updated_at
   #   end
+  add terms_of_use_path, changefreq: 'never'
+  add privacy_policy_path, changefreq: 'never'
+  add contact_path, changefreq: 'never'
 end
