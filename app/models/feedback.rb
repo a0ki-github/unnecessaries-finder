aa41d3edcd1c1c8fe55913a98865ac7d900fd5d7
a0ki-github <aoki.hotaka@gmail.com>
2022-01-16 16:43:52 +0900

    Delete: rails destroy model Feedback

--- app/models/feedback.rb
+++
@@ -1,8 +0,0 @@
-class Feedback < ApplicationRecord
-  enum kind: { 'アプリについてのご意見やご質問': 0, '不具合・エラー報告': 1, 'その他': 2 }
-
-  validates :kind, presence: true
-  validates :content, presence: true, length: { maximum: 65535 }
-
-  before_create -> { self.uuid = SecureRandom.uuid }
-end
